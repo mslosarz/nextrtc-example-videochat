e4522e71196ca4bfaf325bc465bc37f590bda9de
Marcin Ślósarz <m.slosarz@ahej.pl>
2016-02-09 00:26:07 +0100

    Changed version nextrtc-signaling-server to 0.0.2

--- src/main/java/org/nextrtc/examples/videochat/EndpointConfig.java
+++ src/main/java/org/nextrtc/examples/videochat/EndpointConfig.java
@@ -1,11 +1,15 @@
 package org.nextrtc.examples.videochat;
 
 import org.nextrtc.signalingserver.NextRTCConfig;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.concurrent.ScheduledExecutorFactoryBean;
+
+import java.util.concurrent.ScheduledExecutorService;
 
 @Configuration
 @Import(NextRTCConfig.class)
 public class EndpointConfig {
-
 }
