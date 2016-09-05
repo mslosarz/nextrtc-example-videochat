995830a463f520366d33b523a7e7c5e941c3eab1
Marcin Ślósarz <m.slosarz@ahej.pl>
2016-09-05 14:08:01 +0200

    videochat updated to version 0.0.3 Readme updated

--- src/main/java/org/nextrtc/examples/videochat/SampleWebStaticApplication.java
+++ src/main/java/org/nextrtc/examples/videochat/SampleWebStaticApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 @EnableAutoConfiguration
@@ -19,5 +21,15 @@ public class SampleWebStaticApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SampleWebStaticApplication.class);
+	}
+
+	@Bean
+	public MyEndpoint myEndpoint() {
+		return new MyEndpoint();
+	}
+
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
 	}
 }
